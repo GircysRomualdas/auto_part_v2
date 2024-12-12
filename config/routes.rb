8b63928d5effa6361a1b7e283ac62da070e3b068
Romas <r2udis@gmail.com>
2024-12-12 17:07:58 +0000

    add admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :customers
+  devise_for :sellers
+  devise_for :admins
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
   root "home#index"
+
+  get "admin", to: "admin#index"
 end
